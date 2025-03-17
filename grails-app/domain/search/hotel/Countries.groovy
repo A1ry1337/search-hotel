65984ef7f64728b988c60857906a1d9d69c7c59e
a1ry1337 <mixail-aristov07072003@mail.ru>
2025-03-17 17:33:55 +0500

    Фикс невозможности удалить страну если она связана с отелем

--- grails-app/domain/search/hotel/Countries.groovy
+++ grails-app/domain/search/hotel/Countries.groovy
@@ -4,12 +4,18 @@ class Countries {
     String name
     String cityCapital
 
+    static hasMany = [hotels: Hotels]
+
     static constraints = {
         name maxSize: 255
         cityCapital maxSize: 128
 
         name nullable: false
         cityCapital nullable: false
+    }
+
+    static mapping = {
+        hotels cascade: "all-delete-orphan"
     }
 
     String toString() {
